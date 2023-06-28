504f261dec67c13a9136ff242a34db0bdd690257
abetoluwani <101352514+cybertolu@users.noreply.github.com>
2023-06-28 13:30:14 +0100

    on colos

--- while.sh
+++ while.sh
@@ -1,20 +1,27 @@
 #!/bin/bash
 
-myvar=1
+# myvar=1
 
-while [ $myvar -le 10 ] # Where le means less than or equal to
+# while [ $myvar -le 10 ] # Where le means less than or equal to
+# do
+#     echo $myvar
+#     myvar=$(( $myvar + 1 )) # incrementing  myvar
+#     sleep 0.5 #more like a time lap kinda thingy
+
+# done
+
+# myvar1=10
+
+# while [ $myvar1 -ge 1 ]
+# do
+#     echo $myvar1
+#     myvar1=$(( $myvar1 - 1 ))
+#     sleep 0.7 # a feature that acts like time
+# done # always use done at the end of a while loop
+
+while [ -f /home/softwareuser/Documents/bash/update.log ]
 do
-    echo $myvar
-    myvar=$(( $myvar + 1 )) # incrementing  myvar
-    sleep 0.5 #more like a time lap kinda thingy
-
+    echo "The Update File Exists"
 done
 
-myvar1=10
-
-while [ $myvar1 -ge 1 ]
-do
-    echo $myvar1
-    myvar1=$(( $myvar1 - 1 ))
-    sleep 0.7
-done # always use done at the end of a while loop
+echo "The file Does Not Exit"
